38bc09114f1827793a06f237517000b7e79ec6a1
Marc Rousavy <me@mrousavy.com>
2022-05-10 13:09:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,42 @@ import {
   StyleSheet,
   useColorScheme,
 } from 'react-native';
+import {getFromAsyncStorage} from './storages/AsyncStorage';
+import {getFromMMKV} from './storages/MMKV';
+import {getFromSQLite} from './storages/SQLite';
+
+const iterations = 1000;
+
+async function benchmark(
+  label: string,
+  fn: () => unknown | Promise<unknown>,
+): Promise<number> {
+  try {
+    console.log(`Starting Benchmark "${label}"...`);
+    const start = performance.now();
+    for (let i = 0; i < iterations; i++) {
+      const r = fn();
+      if (r instanceof Promise) {
+        await r;
+      }
+    }
+    const end = performance.now();
+    const diff = end - start;
+    console.log(`Finished Benchmark "${label}"! Took ${diff.toFixed(4)}ms!`);
+    return end - start;
+  } catch (e) {
+    console.error(`Failed Benchmark "${label}"!`, e);
+    return 0;
+  }
+}
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const runBenchmarks = useCallback(() => {
-    console.log('no.');
+  const runBenchmarks = useCallback(async () => {
+    await benchmark('MMKV        ', getFromMMKV);
+    await benchmark('AsyncStorage', getFromAsyncStorage);
+    await benchmark('SQLite      ', getFromSQLite);
   }, []);
 
   return (
