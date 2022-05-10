a66ae00fc278da96774c1f3f43f017c6985aebd9
Marc Rousavy <me@mrousavy.com>
2022-05-10 13:10:32 +0200

    Fix SQLite benchmark

--- src/storages/SQLite.ts
+++ src/storages/SQLite.ts
@@ -9,22 +9,24 @@ if (dbOpenResult.status) {
   console.error('SQLite Database could not be opened');
 }
 
-let result = sqlite.executeSql(db, 'DROP TABLE IF EXISTS Values', []);
+let result = sqlite.executeSql(db, 'DROP TABLE IF EXISTS Benchmark', []);
 if (!result.status) {
-  console.log('Created Table Values');
+  console.log('Created Table Benchmark');
 } else {
   console.error(result);
 }
 
 sqlite.executeSql(
   db,
-  'CREATE TABLE IF NOT EXISTS Values(value VARCHAR(30))',
+  'CREATE TABLE IF NOT EXISTS Benchmark(value VARCHAR(30))',
   [],
 );
-sqlite.executeSql(db, 'INSERT INTO Values (value) VALUES (:value)', ['hello']);
+sqlite.executeSql(db, 'INSERT INTO Benchmark (value) VALUES (:value)', [
+  'hello',
+]);
 
 export function getFromSQLite(): string | undefined {
-  let {status, rows} = sqlite.executeSql(db, 'SELECT * FROM `values`', []);
+  let {status, rows} = sqlite.executeSql(db, 'SELECT * FROM `Benchmark`', []);
   if (rows == null || rows.length < 1) {
     throw new Error(`Failed to get Values! ${JSON.stringify(status)}`);
   }
