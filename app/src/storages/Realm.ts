abd8aee86e9fbd0553bb23714f8c0c12d64e816a
Kræn Hansen <kraen.hansen@mongodb.com>
2024-01-11 14:19:50 +0100

    Opening Realm and creating object before benchmark

--- app/src/storages/Realm.ts
+++ app/src/storages/Realm.ts
@@ -9,27 +9,19 @@ const TestSchema = {
   primaryKey: '_id',
 };
 
-const realm = (async () => {
-  const r = await Realm.open({
-    path: 'myrealm',
-    schema: [TestSchema],
-  });
-  r.write(() => {
-    r.deleteAll();
-    r.create('Test', {
-      _id: 1,
-      value: 'hello',
-    });
-  });
-  return r;
-})();
+const realm = new Realm({
+  schema: [TestSchema],
+});
 
-export async function getFromRealm(): Promise<string> {
-  const r = await realm;
-  const object = r.objectForPrimaryKey('Test', 1);
-  if (object == null || typeof object.value !== 'string') {
-    throw new Error('RealmDB: Object not found!');
-  }
+realm.write(() => {
+  realm.deleteAll();
+  realm.create('Test', {
+    _id: 1,
+    value: 'hello',
+  });
+});
 
+export function getFromRealm() {
+  const object = realm.objectForPrimaryKey('Test', 1);
   return object.value;
 }
